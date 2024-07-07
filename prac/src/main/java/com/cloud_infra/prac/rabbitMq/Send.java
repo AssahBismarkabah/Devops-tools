123b582aacb546e92e6b333c224ac76c79f4d919
AssahBismarkabah <assah9799@gmial.com>
2024-07-07 17:03:42 +0100

    added keystone config

--- prac/src/main/java/com/cloud_infra/prac/rabbitMq/Send.java
+++ prac/src/main/java/com/cloud_infra/prac/rabbitMq/Send.java
@@ -1,5 +1,25 @@
 package com.cloud_infra.prac.rabbitMq;
 
+import com.rabbitmq.client.*;
+
 public class Send {
     
+    private final static String QUEUE_NAME = "hello";
+    public static void main(String[] argv) throws Exception {
+        
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost("localhost");
+    try (Connection connection = factory.newConnection();
+         Channel channel = connection.createChannel()) {
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        String message = "Hello World!";
+
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+    }
 }
